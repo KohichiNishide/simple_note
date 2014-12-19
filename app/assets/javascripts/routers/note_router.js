38c8457bf73069f4692ae8c13924f416a2d5961c
Koichi Nishide <kohichi.nishide@gmail.com>
2014-12-19 18:33:57 +0900

    Implement edit note view

--- app/assets/javascripts/routers/note_router.js
+++ app/assets/javascripts/routers/note_router.js
@@ -16,24 +16,38 @@ s.Routers.NoteRouter = Backbone.Router.extend({
     ".*": "indexNotes"
   },
   indexNotes: function(){
-    this.notes || (this.notes = new s.Collections.NoteCollection());
     if (this.currentView == null ) {
       this.currentView.remove();
     }
+    this.notes || (this.notes = new s.Collections.NoteCollection());
     this.currentView = new s.Views.Notes.IndexView(collection: this.notes);
     return this.notes.fetch(reset: true);
   },
   newNote: function(){
-    this.note = new s.Models.Note();
     if (this.currentView == null ) {
       this.currentView.remove();
     }
-    this.currentView = new s.Views.NewView(model: this.note);
+    this.note = new s.Models.Note();
+    this.currentView = new s.Views.NoteView(model: this.note);
     this.listenTo(this.currentView, 'clickSubmit', (function(_this) {
       return function() {
         _this.note.save();
       };
     })(this));
     return this.currentView.render();      
+  }
+  showNote: function() {
+    if (this.currentView) {
+      this.currentView.remove();
+    }
+    this.note = new s.Views.Notes.NoteView({
+      model: this.note
+    });
+    this.listenTo(this.currentView, 'clickSubmit', (function(_this) {
+      return function() {
+        return _this.note.save();
+      };
+    })(this));
+    return this.currentView.render();
   }
 });
---
+++ app/assets/javascripts/views/notes/note_view.js
@@ -0,0 +1,35 @@
+var s, _base;
+
+if (window.SimpleNote == null) {
+  window.SimpleNote = {
+    View: {}
+  };
+}
+
+s = window.SimpleNote;
+
+if ((_base = s.Views).Notes == null) {
+  _base.Notes = {};
+}
+
+s.Views.Notes.NoteView = Backbone.View.extend({
+  className: 'new-note',
+  template: JST['notes/note'],
+  events: {
+    'click .submit-note-form' : 'submit'
+  },
+  binding: {
+    '#input-note-title' : 'title',
+    '#input-note-raw_body' : 'raw_body'
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    $('#main').html(this.el);
+    this.stickit();
+    return this;
+  },
+  submit: function(e) {
+    e.preventDefault();
+    return this.trigger('clickSubmit');
+  }
+});
