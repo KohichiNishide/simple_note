f69b8c2ca719d9c35e7ef23553dcfeb23a78af0b
Koichi Nishide <kohichi.nishide@gmail.com>
2014-12-19 19:58:18 +0900

    Redirect to index after create note

--- app/assets/javascripts/routers/note_router.js
+++ app/assets/javascripts/routers/note_router.js
@@ -15,11 +15,13 @@ s.Routers.NoteRouter = Backbone.Router.extend({
     "notes": "indexNotes",
     ".*": "indexNotes"
   },
+  initialize: function(options) {
+    return this.notes = new s.Collections.NoteCollection();
+  },
   indexNotes: function(){
     if (this.currentView == null ) {
       this.currentView.remove();
     }
-    this.notes || (this.notes = new s.Collections.NoteCollection());
     this.currentView = new s.Views.Notes.IndexView(collection: this.notes);
     return this.notes.fetch(reset: true);
   },
@@ -28,26 +30,41 @@ s.Routers.NoteRouter = Backbone.Router.extend({
       this.currentView.remove();
     }
     this.note = new s.Models.Note();
-    this.currentView = new s.Views.NoteView(model: this.note);
-    this.listenTo(this.currentView, 'clickSubmit', (function(_this) {
+    this.__renderNoteView();
+    return this.listenTo(this.note, 'sync', (function(_this) {
       return function() {
-        _this.note.save();
+        return _this.navigate("notes", true);
       };
     })(this));
-    return this.currentView.render();      
-  }
-  showNote: function() {
+  },
+  showNote: function(id) {
     if (this.currentView) {
       this.currentView.remove();
     }
-    this.note = new s.Views.Notes.NoteView({
+    this.note = this.notes.get(id);
+    if (this.note) {
+      return this.__renderNoteView();
+    } else {
+      this.note = new s.Models.Note({
+        id: id
+      });
+      return this.note.fetch({
+        success: (function(_this){
+          return function() {
+            return _this.__renderNoteView();
+          };
+        })(this) 
+      });
+    }
+  },
+  __renderNoteView: function() {
+    this.currentView = new s.Views.Notes.NoteView({
       model: this.note
     });
-    this.listenTo(this.currentView, 'clickSubmit', (function(_this) {
+    return this.listenTo(this.currentView, 'clickSubmit', (function(_this) {
       return function() {
         return _this.note.save();
       };
     })(this));
-    return this.currentView.render();
   }
 });
