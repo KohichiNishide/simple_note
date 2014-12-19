a062655700d6852b8b5db94875d51c263b152978
Koichi Nishide <kohichi.nishide@gmail.com>
2014-12-19 11:18:08 +0900

    Implement data bindings by Backbone.stickit

--- app/assets/javascripts/views/notes/new_view.js
+++ app/assets/javascripts/views/notes/new_view.js
@@ -15,9 +15,15 @@ if ((_base = s.Views).Notes == null) {
 s.Views.Notes.NewView = Backbone.View.extend({
   className: 'new-note',
   template: JST['notes/new'],
+  
+  binding:
+    '#input-note-title' : 'title'
+    '#input-note-raw_body' : 'raw_body'
+
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
     $('#main').html(this.el);
+    this.stickit;
     return this;
   }
 });
